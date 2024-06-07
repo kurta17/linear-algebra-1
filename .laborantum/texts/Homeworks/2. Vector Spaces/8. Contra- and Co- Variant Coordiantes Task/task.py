fc36840da19156823ade0cc1d0674000cc417818
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-06-07 17:49:20 +0700

    wp

--- .laborantum/texts/Homeworks/2. Vector Spaces/8. Contra- and Co- Variant Coordiantes Task/task.py
+++ .laborantum/texts/Homeworks/2. Vector Spaces/8. Contra- and Co- Variant Coordiantes Task/task.py
@@ -1,14 +1,18 @@
-answer = {
-    'x_a_contra': [6., 5., 3.],
-    'x_a_co': [14.,28.,42.],
-    'x_b_contra': [1],
-    'x_b_co': [1],
-    'y_a_contra': [1],
-    'y_a_co': [1],
-    'y_b_contra': [1],
-    'y_b_co': [1]
+answer = { 
+    'x_a_contra': [-1., -1., 3.],
+    'x_a_co': [1.,3.,6.],
+    'x_b_contra': [1.,1.,1.],
+    'x_b_co': [6.,5.,3.],
+    'y_a_contra': [-1.,-1.,5.],
+    'y_a_co': [3.,7.,12.],
+    'y_b_contra': [3.,1.,1.],
+    'y_b_co': [12.,9.,5.],
 }
-import numpy as np
-x = np.array([1, 2, 3])
-a = np.array([[1,1,1], [0,1,1], [0,0,1]])
+
+
+
+
+
+
+
 
