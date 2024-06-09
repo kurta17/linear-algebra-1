2698292c0e62951c2182a77e23ca657bc07fe33b
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-06-09 17:31:09 +0700

    wp

--- .laborantum/texts/Homeworks/2. Vector Spaces/12. Covariant Coordinates Numpy I/task.py
+++ .laborantum/texts/Homeworks/2. Vector Spaces/12. Covariant Coordinates Numpy I/task.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
+# In[1]:
 
 
 get_ipython().run_line_magic('reload_ext', 'autoreload')
@@ -20,50 +20,10 @@ if index > 0:
 
 os.chdir(path)
 
-get_ipython().system('{sys.executable} -m pip -q install --user numpy json-tricks torch jupyter nbconvert')
+get_ipython().system('{sys.executable} -m pip -q install numpy json-tricks torch jupyter nbconvert')
 
 
-# In[8]:
-
-
-import numpy
-import json_tricks
-import os
-
-numpy.random.seed(42)
-
-debug_cases = []
-for index in range(20):
-    B_shape = numpy.random.randint(1, 10, size=[2])
-    B_shape.sort(axis=0)
-    B_shape = B_shape[::-1]
-    B = numpy.random.randint(-5, 5, size=B_shape)
-    x_shape = B_shape[-1:]
-    x = numpy.random.randint(-5, 5, size=x_shape)
-    # if numpy.random.randn(1) < 0:
-        # A[:, -1] = A[:, :-1] @ numpy.random.randint(-5, 5, size=[A.shape[-1] - 1])
-    debug_cases.append({'B': B, 'x': x})
-
-os.makedirs('testcases', exist_ok=True)
-with open('testcases/debug_cases.json', 'w+') as fin:
-    fin.write(json_tricks.dumps(debug_cases))
-
-public_cases = []
-for index in range(100):
-    B_shape = numpy.random.randint(1, 10, size=[2])
-    B_shape.sort(axis=0)
-    B_shape = B_shape[::-1]
-    B = numpy.random.randint(-5, 5, size=B_shape)
-    x_shape = B_shape[-1:]
-    x = numpy.random.randint(x_shape)
-    
-    public_cases.append({'B': B, 'x': x})
-
-with open('testcases/public_cases.json', 'w+') as fin:
-    fin.write(json_tricks.dumps(public_cases))
-
-
-# In[10]:
+# In[3]:
 
 
 import json_tricks
@@ -76,16 +36,21 @@ public_cases = json_tricks.load(
     str(path / 'testcases' / 'public_cases.json'))
 
 
-# In[11]:
+# In[4]:
 
 
 import numpy as np
 
 def get_covariant_coordinates(B, x):
-    return (B * x.reshape([1, -1])).sum(axis=1)
+    ## B * res = x
+    if B.shape[0] != B.shape[1]:
+        raise ValueError("Matrix B must be square")
+    
+    res = np.linalg.solve(B, x)
+    return res
 
 
-# In[12]:
+# In[13]:
 
 
 import time
@@ -96,10 +61,4 @@ debug_result = [get_covariant_coordinates(**x) for x in debug_cases]
 answer = [get_covariant_coordinates(**x) for x in public_cases]
 
 print(time.time() - start, '<- Elapsed time')
-
-
-# In[ ]:
-
-
-
 
--- .laborantum/texts/Homeworks/2. Vector Spaces/9. Contravariant Coordinates Numpy/task.py
+++ .laborantum/texts/Homeworks/2. Vector Spaces/9. Contravariant Coordinates Numpy/task.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 get_ipython().run_line_magic('reload_ext', 'autoreload')
@@ -23,46 +23,7 @@ os.chdir(path)
 get_ipython().system('{sys.executable} -m pip -q install --user numpy json-tricks torch jupyter nbconvert')
 
 
-# In[8]:
-
-
-import numpy
-import json_tricks
-import os
-
-numpy.random.seed(42)
-
-debug_cases = []
-for index in range(20):
-    A_shape = numpy.random.randint(1, 10, size=[2])
-    A = numpy.random.randint(-5, 5, size=A_shape)
-    B_shape = numpy.random.randint(1, 10, size=[2])
-    B_shape[0] = A_shape[0]
-    C = numpy.random.randint(-5, 5, size=A_shape)
-    
-    # if numpy.random.randn(1) < 0:
-        # A[:, -1] = A[:, :-1] @ numpy.random.randint(-5, 5, size=[A.shape[-1] - 1])
-    debug_cases.append({'B': A, 'C': C})
-
-os.makedirs('testcases', exist_ok=True)
-with open('testcases/debug_cases.json', 'w+') as fin:
-    fin.write(json_tricks.dumps(debug_cases))
-
-public_cases = []
-for index in range(100):
-    A_shape = numpy.random.randint(1, 10, size=[2])
-    A = numpy.random.randn(*A_shape)
-    B_shape = numpy.random.randint(1, 10, size=[2])
-    B_shape[0] = A_shape[0]
-    C = numpy.random.randn(*A_shape)
-    
-    public_cases.append({'B': A, 'C': C})
-
-with open('testcases/public_cases.json', 'w+') as fin:
-    fin.write(json_tricks.dumps(public_cases))
-
-
-# In[9]:
+# In[2]:
 
 
 import json_tricks
@@ -75,16 +36,19 @@ public_cases = json_tricks.load(
     str(path / 'testcases' / 'public_cases.json'))
 
 
-# In[10]:
+# In[3]:
 
 
 import numpy as np
 
 def vectors_from_contravariant_coords(B, C):
-    return B.T @ C
+    B = np.expand_dims(B, 1)
+    C = np.expand_dims(C, 2)
+    res = np.sum(B * C, axis=0)
+    return res
 
 
-# In[12]:
+# In[4]:
 
 
 import time
@@ -95,17 +59,4 @@ debug_result = [vectors_from_contravariant_coords(**x) for x in debug_cases]
 answer = [vectors_from_contravariant_coords(**x) for x in public_cases]
 
 print(time.time() - start, '<- Elapsed time')
-
-
-# In[10]:
-
-
-print(debug_result)
-print(answer)
-
-
-# In[ ]:
-
-
-
 
