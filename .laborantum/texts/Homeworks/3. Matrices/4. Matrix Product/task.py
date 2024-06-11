1bf52d02778caae944e43f06e8294e1d619a94d3
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-06-11 16:54:30 +0700

    new homework ahhhh

--- .laborantum/texts/Homeworks/3. Matrices/4. Matrix Product/task.py
+++ .laborantum/texts/Homeworks/3. Matrices/4. Matrix Product/task.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 answer = {
-    'task1': 1,
-    'task2': 2,
+    'task1': np.array([
+              [30, 36,42],
+              [66, 81, 96],
+              [102, 126, 150]
+              ]).astype("int64"),
+    'task2': np.array([]).astype("int64"),
     'task3': 3
 }
