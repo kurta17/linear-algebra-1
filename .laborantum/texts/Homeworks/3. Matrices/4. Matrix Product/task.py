00cfe86dee7b1145d1ba9662197490f82a76f201
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-06-11 17:07:36 +0700

    wp

--- .laborantum/texts/Homeworks/3. Matrices/4. Matrix Product/task.py
+++ .laborantum/texts/Homeworks/3. Matrices/4. Matrix Product/task.py
@@ -6,6 +6,16 @@ answer = {
               [66, 81, 96],
               [102, 126, 150]
               ]).astype("int64"),
-    'task2': np.array([]).astype("int64"),
-    'task3': 3
+    'task2': np.array([
+                [37,54],
+                [81,118]
+                ]).astype("int64"),
+    'task3': np.array([215, 450]).astype("int64")   
 }
+
+A = np.array([[1,2,3,4,5],[6,7,8,9,0]])
+B = np.array([[1,2],[3,4],[5,6],[7,8],[9,0]])
+print(np.dot(A,B))
+K = np.dot(A,B)
+x = np.array([1,2])
+print(np.dot(K,x))
