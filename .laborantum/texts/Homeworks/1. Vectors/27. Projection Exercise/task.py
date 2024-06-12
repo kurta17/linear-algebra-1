13d1f34def390f73e65f08dc4fed63dc32f17cec
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-06-12 18:06:09 +0700

    wp

--- .laborantum/texts/Homeworks/1. Vectors/27. Projection Exercise/task.py
+++ .laborantum/texts/Homeworks/1. Vectors/27. Projection Exercise/task.py
@@ -1,14 +1,10 @@
 import numpy as np
 
 answer = {
-    'proj_a_b': np.array([0.5, 0.5, 1.5, 1.5, 2.0]).astype("int64"),
-    'orth_a_b': np.array([5.5, 2.5, 0.5, -0.5, -2.0]).astype("int64"),
-    'proj_b_a': np.array([2.16, 1.08, 0.72, 0.36, 0.0]).astype("int64"),
-    'orth_b_a': np.array([
-            -1.1600000000000001,
-            -0.08000000000000007,
-            2.2800000000000002,
-            2.64,
-            4.0,
-        ]).astype("int64")
+    'proj_a_b': np.array([0, 0, 1, 1, 2]).astype("int32"),
+    'orth_a_b': np.array([5, 2, 0, 0, -2]).astype("int64"),
+    'proj_b_a': np.array([2, 1, 0, 0, 0]).astype("int64"),
+    'orth_b_a': np.array([-1, 0, 2, 2, 4]).astype("int64")
 }
+
+
