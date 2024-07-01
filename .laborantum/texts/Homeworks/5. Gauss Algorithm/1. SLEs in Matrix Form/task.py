5e99546a02e363d737ecbdcb98cb5462e612b29b
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-07-02 02:32:59 +0700

    wp

--- .laborantum/texts/Homeworks/5. Gauss Algorithm/1. SLEs in Matrix Form/task.py
+++ .laborantum/texts/Homeworks/5. Gauss Algorithm/1. SLEs in Matrix Form/task.py
@@ -3,57 +3,76 @@ import numpy as np
 answer = {
     'task_1': {
         'A': np.array([
-            [0]
-        ]),
-        'b': np.array([[0]]).T,
+            [1,1],
+            [2,1]
+        ]).astype("int64"),
+        'b': np.array([[17,10]]).T,
         'inv_A': np.array([
-            [0]
-        ]),
+            [-1,1],
+            [2,-1]
+        ]).astype("int64"),
         'L': np.array([
-            [0]
-        ]),
+            [1,0],
+            [2,-1]
+        ]).astype("int64"),
         'U': np.array([
-            [0]
-        ]),
-        'x': np.array([[0]]).T,
+            [1,1],
+            [0,1]
+        ]).astype("int64"),
+        'x': np.array([[-7,24]]).T,
     },
     'task_2': {
         'A': np.array([
-            [0]
-        ]),
-        'b': np.array([[0]]).T,
+            [1,5],
+            [2,9]
+        ]).astype("int64"),
+        'b': np.array([[2,4]]).T,
         'inv_A': np.array([
-            [0]
-        ]),
+            [-9,5],
+            [2,-1]
+        ]).astype("int64"),
         'L': np.array([
-            [0]
-        ]),
+            [1,0],
+            [2,-1]
+        ]).astype("int64"),
         'U': np.array([
-            [0]
-        ]),
-        'x': np.array([[0]]).T,
+            [1,5],
+            [0,1]
+        ]).astype("int64"),
+        'x': np.array([[2,0]]).T,
     },
     'task_3': {
         'A': np.array([
-            [0]
-        ]),
-        'b': np.array([[0]]).T,
+            [1, 0, 1],  
+            [2, 1, -1],
+            [3, 1, -2]  
+        ]).astype("int64"),
+        'b': np.array([[3,2,3]]).T,
         'inv_A': np.array([
-            [0]
+            [0.5, 2.5, -1.5],
+            [-0.5, 2.5, -1.5],
+            [0.5, 0.5, -0.5]
         ]),
         'L': np.array([
-            [0]
-        ]),
+            [1, 0, 0],
+            [-2, 1, 0],
+            [1, 1, -1]   
+        ]).astype("int64"),
         'U': np.array([
-            [0]
-        ]),
-        'x': np.array([[0]]).T,
+            [1, 0, 0],
+            [0,1,-3],
+            [0, 0, 2]
+        ]).astype("int64"),
+        'x': np.array([[2, -1, 1]]).T,
     },
     'task_4': {
         'A': np.array([
-            [0]
+            [1,1,1,1],
+            [2,1,1,1],
+            [3,2,1,1],
+            [4,3,2,1]
         ]),
-        'b': np.array([[0]]).T,
+        'b': np.array([[4,5,7,10]]).T,
         'inv_A': np.array([
             [0]]),
         'L': np.array([
@@ -63,5 +82,5 @@ answer = {
             [0]
         ]),
         'x': np.array([[0]]).T,
-    },
+    }
 }
